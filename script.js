d289446f9b94a4dd6c82900636fc523c7bd6ebdb
fufigop <lamiaaismail3490@gmail.com>
2025-07-19 23:42:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+js
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav ul');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+```
